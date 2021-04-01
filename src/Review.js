4e81c6c7661bd3a2795c24e8353ca91f9c77e107
AbdelRahman Reda <veguslive@gmail.com>
2021-04-01 02:12:38 +0200

    Display one person then added prev and next button to change between the displayed person

---
+++ src/Review.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const { id, image, name, job, text } = people[index];
+
+  const nextPerson = (e) => {
+    setIndex(index === people.length - 1 ? 0 : index + 1);
+  };
+
+  const prevPerson = (e) => {
+    setIndex(index === 0 ? people.length - 1 : index - 1);
+  };
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default Review;
